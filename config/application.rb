25151e40144d4b1eafde9f6e62e97b4196c0fdf6
shtan14 <wrzn.inw.snun@gmail.com>
2023-07-02 10:27:29 +0900

    Finished Setting Rspec

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,13 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    # 生成されるテストファイルをRSpecに、使用頻度の高くないSpecファイルが生成されないように設定
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
