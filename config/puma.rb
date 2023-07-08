7ad6ff36ef1dda354ed06686d720e8ca80be5c71
shtan14 <wrzn.inw.snun@gmail.com>
2023-07-08 16:43:14 +0900

    nginx用Dockerfile、neginx.conf、docker-compose.yml更新

--- config/puma.rb
+++ config/puma.rb
@@ -1,49 +1,21 @@
-# Doc:https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-
-workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
-
-
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+# workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
 min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS") { max_threads_count })
 threads min_threads_count, max_threads_count
-
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-preload_app!
-
-
-rackup      DefaultRackup
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-#
-# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
+# preload_app!
+# rackup      DefaultRackup
+# コメントアウト
+# port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RACK_ENV") { "development" }
+# nginx ソケット通信設定追加
+# app_root：__dir__（現在のファイルのディレクトリ）の親ディレクトリを取得
+app_root = File.expand_path('..', __dir__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+plugin :tmp_restart
