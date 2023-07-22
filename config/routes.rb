c84a5c069f9715d2ea9a664082a82547d280a212
shtan14 <wrzn.inw.snun@gmail.com>
2023-07-22 15:16:12 +0900

    routes.rb修正　バックエンドECSデプロイ完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       # api test action
       resources :hello, only:[:index]
+        # ヘルスチェック用のルートを追加
+      # get '/healthcheck', to: 'api/v1/healthcheck#index'
+      get :healthcheck, to: 'healthcheck#index'
     end
   end
 end
