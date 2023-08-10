b589dec37346284e808f090f9d0df7a6f04eaf82
shtan14 <wrzn.inw.snun@gmail.com>
2023-08-10 20:27:31 +0900

    email_custom_validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true,
                    length: { maximum: 30, allow_blank: true }
   validates :email, presence: true,
-                   email: { allow_blank: true }
+                    email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\w-]+\z/
   validates :password, presence: true,
@@ -22,21 +22,21 @@ class User < ApplicationRecord
   class << self
     # emailからアクティブなユーザーを返す
     def find_by_activated(email)
-      find_by(email: email, activated: true)
+      find_by(email:, activated: true)
     end
   end
   # class method end #########################
 
   # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
   def email_activated?
-    users = User.where.not(id: id)
+    users = User.where.not(id:)
     users.find_by_activated(email).present?
   end
-  
+
   private
 
   # email小文字化
-  def downcase_email
-    self.email.downcase! if email
-  end
+    def downcase_email
+      email&.downcase!
+    end
 end
