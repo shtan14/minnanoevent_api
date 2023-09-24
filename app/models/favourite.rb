4800e6f68d5ec754eb44de6f8b3e1707feaff676
shtan14 <wrzn.inw.snun@gmail.com>
2023-09-24 11:46:34 +0900

    fix_#23_favourite,user,eventモデルの関連付けを実装

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,2 +1,17 @@
 class Favourite < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+
+  after_create :increment_favourites_count
+  after_destroy :decrement_favourites_count
+
+  private
+
+  def increment_favourites_count
+    event.increment!(:favourites_count)
+  end
+
+  def decrement_favourites_count
+    event.decrement!(:favourites_count)
+  end
 end
