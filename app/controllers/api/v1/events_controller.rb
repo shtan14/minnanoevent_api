e693e578a646173d1de1c2f65856f24e4ac67071
shtan14 <wrzn.inw.snun@gmail.com>
2023-10-03 20:59:58 +0900

    events_controllerコメント追加

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::EventsController < ApplicationController
   # TODO 開発時xhr_request?を無効化。最後は有効化させる。
   skip_before_action :xhr_request?, only: [:index, :show]
+  # イベント一覧を取得
   def index
     events = Event.all
     render json: events
