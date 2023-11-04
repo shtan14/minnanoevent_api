b31a9ccc00938358da0056e4c7a91a3ba37aeea7
shtan14 <wrzn.inw.snun@gmail.com>
2023-11-04 12:02:04 +0900

    user_idがある場合はそのユーザーに紐づくイベントのみを取得するようにeventscontrollerのindexアクションを修正

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::EventsController < ApplicationController
   # TODO 開発時xhr_request?を無効化。最後は有効化させる。
   skip_before_action :xhr_request?, only: [:index, :show]
   def index
-    events = Event.includes(:categories, :event_images).all
+    # クエリパラメータにuser_idがある場合は、そのユーザーに紐づくイベントのみを取得
+    if params[:user_id]
+      events = Event.includes(:categories, :event_images).where(user_id: params[:user_id])
+    else
+      events = Event.includes(:categories, :event_images).all
+    end
+
     render json: events, include: [:categories, :event_images]
   end
 
