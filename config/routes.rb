ffd7a5b93ba1ba621b9ebd07fb26dc5f76f09025
shtan14 <wrzn.inw.snun@gmail.com>
2023-11-20 20:46:54 +0900

    userコントローラーのdestroyに対応するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
         post :refresh, on: :collection
         delete :destroy, on: :collection
       end
+
       get "healthcheck", to: "healthcheck#index"
+
       resources :categories, only: %i[index show]
-      resources :users, only: %i[show create]
+
+      resources :users, only: %i[show create] do
+        delete :destroy, on: :member
+      end
+
       resources :events, only: %i[index show] do
         resources :event_images, only: [:index]
         resources :comments, only: [:index]
