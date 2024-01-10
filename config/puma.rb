c6fe5d27e8c93584b3f715393758300765ee6fd1
shtan14 <wrzn.inw.snun@gmail.com>
2024-01-10 18:37:14 +0900

    Fix不要なコメントアウトを削除

--- config/puma.rb
+++ config/puma.rb
@@ -1,27 +1,9 @@
-# workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
-
-# max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
 max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
 min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS") { max_threads_count })
 threads min_threads_count, max_threads_count
 
-# preload_app!
-# rackup      DefaultRackup
-
-# port ENV.fetch("PORT") { 3000 } ソケット通信に変更のためコメントアウト
 environment ENV.fetch("RAILS_ENV") { "development" } # RACK_ENV→RAILS_ENV
 
-# nginx ソケット通信設定追加
-# app_root：__dir__（現在のファイルのディレクトリ）の親ディレクトリを取得
-# app_root = File.expand_path('..', __dir__)
-# bind "unix://#{app_root}/tmp/sockets/puma.sock"
-# stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
-
 bind "unix:///app/tmp/sockets/puma.sock"
-# stdout_redirect "/app/log/puma.stdout.log", "/app/log/puma.stderr.log", true
-
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection
-# end
 
 plugin :tmp_restart
